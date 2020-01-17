d15ea15b2944b3a769aa64080e937beb2372448d
mrdimonster@gmail.com <mrdimonster@gmail.com>
2020-01-17 18:06:17 +0300

    demo solver

--- hanoi/src/classes/solver.ts
+++ hanoi/src/classes/solver.ts
@@ -4,8 +4,6 @@ export class Solver{
     public stack3: Array<number> = [];
     disksCount: number = 3;
     recursive: boolean = false;
-    from?: Array<number>;
-    to?: Array<number>;
     lastPusher?: number;
     public movesCount: number = 0;
     solved: boolean = false;
@@ -18,13 +16,10 @@ export class Solver{
         this.stack2 = [];
         this.stack3 = [];
         this.movesCount = 0;
-        this.from = undefined;
-        this.to = undefined;
         this.lastPusher = -1;
         this.solved = false;
         this.pauseMs = pauseMs;
-        for(let i = 1; i <= dk; i++)
-            this.stack1.push(i);
+        for(let i = 1; i <= dk; i++) this.stack1.push(i);
     }
 
     async run(): Promise<void>{
@@ -38,49 +33,54 @@ export class Solver{
         let s1 = this.stack1.length > 0 ? this.stack1[0]: 99;
         let s2 = this.stack2.length > 0 ? this.stack2[0]: 99;
         let s3 = this.stack3.length > 0 ? this.stack3[0]: 99;
-
-        this.from = [];
-        this.to = [];
-        if(s1 < 99 && (s1 < s2 || s1 < s3) && this.lastPusher != 1)
-            this.from = this.stack1;
-        else if(s2 < 99 && (s2 < s1 || s2 < s3) && this.lastPusher != 2)
-            this.from = this.stack2;
-        else if(s3 < 99 && (s3 < s1 || s3 < s2) && this.lastPusher !=3)
-            this.from = this.stack3;
+        let from = [];
+        let to = [];
+        let f = -1; 
+        let t =-1;
+        if(s1 < 99 && (s1 < s2 || s1 < s3) && this.lastPusher != 1) f = 1;
+        else if(s2 < 99 && (s2 < s1 || s2 < s3) && this.lastPusher != 2) f = 2;
+        else if(s3 < 99 && (s3 < s1 || s3 < s2) && this.lastPusher !=3) f = 3;
         
-        let popValue = this.from[0];
-        let s1CanPush = (popValue < s1) && this.from != this.stack1;
-        let s2CanPush = (popValue < s2) && this.from != this.stack2;
-        let s3CanPush = (popValue < s3) && this.from != this.stack3;
+        from = this.stack1;
+        if(f == 2) from = this.stack2;
+        else if(f == 3) from = this.stack3;
+        
+        let popValue = from[0];
+        let s1CanPush = (popValue < s1) && f != 1;
+        let s2CanPush = (popValue < s2) && f != 2;
+        let s3CanPush = (popValue < s3) && f != 3;
 
         let pushCount = (s1CanPush ? 1: 0) + (s2CanPush ? 1 : 0) + (s3CanPush ? 1 : 0);
         if(pushCount === 2){
             ///f->r->t (2n disks), f->t->r (2n+1 disks)
             let curPos = 1;
-            if(this.from == this.stack2) curPos = 2;
-            else if(this.from == this.stack3) curPos = 3;
+            if(f == 2) curPos = 2;
+            else if(f == 3) curPos = 3;
 
-            if(curPos === 1) this.to = this.stack2;
-            else if(curPos === 2) this.to = this.stack3;
-            else if(curPos === 3) this.to = this.stack1;
+            if(curPos === 1) t = 2;
+            else if(curPos === 2) t = 3;
+            else if(curPos === 3) t = 1;
         }
         else{
-            this.to = this.stack3;
-            if(s2CanPush) this.to = this.stack2;
-            else if(s1CanPush) this.to = this.stack1;
+            t = 3;
+            if(s2CanPush) t = 2;
+            else if(s1CanPush) t = 1;
         }
 
-        this.from.shift();
-        this.to.unshift(popValue);
+        to = this.stack1;
+        if(t == 2) to = this.stack2;
+        else if(t == 3) to = this.stack3;
+
+        from.shift();
+        to.unshift(popValue);
+
         this.movesCount++;
         if(this.stack2.length == this.disksCount || this.stack3.length == this.disksCount){
             this.solved = true;
             return true;
         }
         else{
-            this.lastPusher = 1;
-            if(this.to === this.stack2) this.lastPusher = 2;
-            else if(this.to === this.stack3) this.lastPusher = 3;
+            this.lastPusher = t;
         }
         return false;
     }
